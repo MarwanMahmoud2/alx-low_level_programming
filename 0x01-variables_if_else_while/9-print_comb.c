dd6dc6eabdd85069489257ef18455fdadf3f3c70
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-17 15:20:31 +0000

    prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-
 /**
- * main - Prints numbers between 0 to 9 with commas and spaces.
+ * main - Prints all possible combinations of single-digit numbers.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
 int num;
-for (num = 0; num <= 9; num++);
+for (num = 0; num <= 9; num++)
 {
 putchar((num % 10) + '0');
 if (num == 9)
-{
+continue;
 putchar(',');
 putchar(' ');
-}
 }
 putchar('\n');
 return (0);
