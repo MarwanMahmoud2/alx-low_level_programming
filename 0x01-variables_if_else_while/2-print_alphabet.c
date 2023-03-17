d0b42cc928466ca1e9cac0bd25cef135c92e87c3
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-17 14:16:36 +0000

    Printing alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
 int main(void)
 {
 char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-int i;
-for (i = 0, i < 26; i++)
+int a;
+for (a = 0; a < 26; a++)
 {
-putchar(alp[i]);
+putchar(alp[a]);
 }
 putchar('\n');
 return (0);
