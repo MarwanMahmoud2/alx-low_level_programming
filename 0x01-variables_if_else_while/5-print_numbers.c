3eaf724cb161bc46431d62958d714f456ffaeaf0
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-17 14:34:50 +0000

    Printing the base of 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints the base of 10.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num[10] = "0123456789";
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar(num[n]);
+}
+putchar('\n');
+return (0);
+}
