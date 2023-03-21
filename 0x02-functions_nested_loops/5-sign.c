3117af00991c644577b34e46e8949b022ab9f32e
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-21 13:34:15 +0000

    Prints sign of a number if its greater or less than zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_sign - prints sign if n greater than zero or less than zero or otherwise
+ *
+ * Return: 1 if greater than zero , 0 if zero , -1 if less than zero
+ * @n: The number to be checked.
+ */
+int print_sigh(int n)
+{
+if (n > 0)
+_putchar([n] is greater than zero);
+return (1);
+else if (n == 0)
+_putchar([n] is zero);
+return (0);
+else
+_putchar([n] is less than zero);
+return (-1);
+}
