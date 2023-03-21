fb433d8d0f6de86b7971b7855ee34093bc5c178e
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-21 12:40:14 +0000

    Using function printing a to z 10x times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,14 @@
  */
 void print_alphabet_x10(void)
 {
-  int r;
-  for (r = 10; r >= 1 ; r--)
-    {
-  char i;
-  for (i = 'a'; i <= 'z'; i++)
-    {
-      _putchar(i);
-    }
-  _putchar('\n');
-    }
+int r;
+for (r = 10; r >= 1 ; r--)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
 }
