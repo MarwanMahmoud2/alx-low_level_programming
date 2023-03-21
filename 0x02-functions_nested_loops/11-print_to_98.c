dd456c5ec23edfedaf81ebe895e5a89c1868eacf
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-21 16:55:21 +0000

    prints numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,14 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-printf("%d", n++);
-printf("%d", n);
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);
 }
 else
 {
-printf("%d", n--);
-printf("%d", n);
+while (n > 98)
+printf("%d, ", n--);
+printf("%d\n", n);
 }
 }
