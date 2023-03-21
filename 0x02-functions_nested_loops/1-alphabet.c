e73c7104fdaa4a11e4e8034cd55eac56711c0d2c
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-21 11:40:11 +0000

    Using function printing a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Using function printing a to z
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+void print_alphabet(void);
+{
+char i;
+for (i = 'a'; i <= 'z'; i++;)
+{
+putchar(a);
+}
+putchar ("\n");
+}
