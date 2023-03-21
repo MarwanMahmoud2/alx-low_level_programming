69a113898f3c293c971be813b7c45a14664ad2e6
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-21 16:50:38 +0000

    prints numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,27 +3,14 @@
 /**
  * print_to_98 - prints from n to 98
  *
- * @n1: The numbers will be entered
- * @n2: The numbers will be entered
- * @n3: The numbers will be entered
+ * @n: The numbers will be entered
  */
-void print_to_98(int n1, int n2, int n3)
+void print_to_98(int n)
 {
-for (n1 = 0; n1 <= 9; n1++)
-{
-_putchar((n1 % 10) + '0');
-_putchar(',');
-_putchar(' ');
+if (n <= 98)
+printf("%d", n++);
+printf("%d", n);
+else
+printf("%d", n--);
+printf("%d", n);
 }
-for (n2 = 1; n2 <= 9; n2++)
-{
-for (n3 = 0; n3 <= 8; n3++)
-{
-_putchar((n2 % 10) + '0');
-_putchar((n3 % 10) + '0');
-_putchar(',');
-_putchar(' ');
-}
-}
-}
-
