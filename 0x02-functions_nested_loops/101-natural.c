c555d6db23af30cc866c7f67670b92fa7e1cda95
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-21 17:18:16 +0000

    natural multiplied of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * main - lists natural numbers below 1024 and multiples of 3 or 5
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i; sum = 0;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+sum += i;
+}
+printf("%d\n", sum);
+return (0);
+}
