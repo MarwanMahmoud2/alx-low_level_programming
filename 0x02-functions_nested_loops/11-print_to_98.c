5c8df5d4cda80c46896b955f7f0d09ff08410fc1
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-21 16:05:43 +0000

    prints numbers to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_to_98 - prints from n to 98
+ *
+ * @n: The number will be entered
+ */
+void print_to_98(int n)
+{
+for (n = 0; n <= 98; n++)
+_putchar(n);
+_putchar(',');
+_putchar(' ');
+}
