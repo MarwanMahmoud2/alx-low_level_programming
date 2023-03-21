fef54adef95917c9fd4f6013f7b1c76d5ed234e4
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-21 11:47:35 +0000

    Using function printing a to z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,6 @@
  * Return - Always 0 (Success)
  */
 void print_alphabt(void);
-int main(void)
-{
-print_alphabet();
-return (0);
-}
-void print_alphabt(void);
 {
 char i;
 for (i = 'a'; i <= 'z'; i++;)
