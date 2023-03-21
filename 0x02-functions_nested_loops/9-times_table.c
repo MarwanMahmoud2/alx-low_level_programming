8e7db46dcee213bad60367129887b6c9f7ead1d5
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-21 15:52:25 +0000

    Prints 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,15 +7,15 @@
 void times_table(void)
 {
 int num, multi, pord;
-for (num = 0; num <=9; num++)
+for (num = 0; num <= 9; num++)
 {
 _putchar('0');
-for (multi = 1; multi <=9; multi++)
+for (multi = 1; multi <= 9; multi++)
 {
 _putchar(',');
 _putchar(' ');
 pord = num * multi;
-if(pord <= 9)
+if (pord <= 9)
 _putchar(' ');
 else
 _putchar((pord / 10) + '0');
