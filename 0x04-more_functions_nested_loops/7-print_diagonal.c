3e34f0994c8a4ad6c3fe62bbaa05154c0f8c7764
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-23 16:12:05 +0000

    function draws numbers of diagonal lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * print_diagonal - draws diagonal line
+ * @n: number of diagonal line
+ */
+void print_diagonal(int n)
+{
+int count;
+if (n > 0)
+{
+for (count = 0; count < n; count++)
+_putchar("\"");
+}
+_putchar('\n');
+}
