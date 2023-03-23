dbb7e102b5361e4cd861c11fb4711300020be81d
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-23 14:58:15 +0000

    function multiplies twi integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * mul - function that multiplies two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ */
+int mul(int a, int b)
+{
+int sum
+sum = a * b
+_putchar(sum);
+}
