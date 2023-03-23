8a6e7b24a29c5f5260afb40bdd2f923a4ab42dff
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-23 17:40:19 +0000

    function draws numbers a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,11 +5,20 @@
  */
 void print_diagonal(int n)
 {
-int count;
+int count, space;
 if (n > 0)
 {
 for (count = 0; count < n; count++)
+{
+for (space = 0; space < count; space++)
+{
+_putchar(' ');
+}
 _putchar('\\');
+if (count > (n - 1))
+continue;
+_putchar('\n');
+}
 }
 _putchar('\n');
 }
