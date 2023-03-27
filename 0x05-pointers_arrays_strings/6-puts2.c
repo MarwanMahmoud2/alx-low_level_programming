9ddd2a6b7a3b1e9eb9f4dcb25831b44d09b9604c
MarwanMahmoud2 <20220985@tiba.edu.eg>
2023-03-27 15:14:42 +0000

    Adding 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * puts2 - Function prints a reversed string
+ *
+ * @str: the string.
+ */
+void puts2(char *str)
+{
+int i = 0;
+while (i >= 0)
+{
+if (str[i] == '\0')
+{
+_putchar('\n');
+break;
+}
+if (str[i] % 2 == 0)
+{
+ _putchar(str[i]);
+i++
+}
+}
+}
